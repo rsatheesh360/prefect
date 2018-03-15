332fc91a9f8001e5a42a858a6675cefa76b6f51c
R.Satheesh <36471895+rsatheesh360@users.noreply.github.com>
2018-03-15 17:54:34 +0530

    prefect number

---
+++ .gitignore/prefect.py
@@ -0,0 +1,7 @@
+import math
+number=int(input("enter the num:"))
+if (math.sqrt(number)-int(math.sqrt(number))):
+    print(number,("is not a perfect square"))
+else:
+    print(number,("is a perfect square"))
+    print(n)
